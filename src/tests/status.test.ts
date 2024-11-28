bd5368e4a9693feba0dc23a54dc039949eabdb57
Leonardo Nunes <leonunesbs@gmail.com>
2024-11-28 20:32:00 -0300

    feat: add vitest

---
+++ src/tests/status.test.ts
@@ -0,0 +1,6 @@
+import { describe, it } from "vitest";
+
+describe("Tests database connection", () => {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  it("should connect to the database", () => {});
+});
