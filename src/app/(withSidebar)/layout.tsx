da047609ab23dc998e9faefcbe0f2b569c26a6da
Leonardo Nunes <leonunesbs@gmail.com>
2024-11-30 18:34:59 -0300

    feat: protect dashboard routes

--- src/app/(withSidebar)/layout.tsx
+++ src/app/(withSidebar)/layout.tsx
@@ -3,25 +3,31 @@ import {
   SidebarProvider,
   SidebarTrigger,
 } from "~/components/ui/sidebar";
+import { auth, signOut } from "~/server/auth";
 
+import { AppSidebar } from "~/components/organisms/app-sidebar";
+import { Button } from "~/components/ui/button";
+import { CustomBreadcrumbs } from "~/components/molecules/custom-breadcrumbs";
 import Form from "next/form";
-import { cookies } from "next/headers";
 import Image from "next/image";
 import Link from "next/link";
 import { MdLogout } from "react-icons/md";
-import { ThemeToggle } from "~/components/atoms/theme-toggle";
-import { CustomBreadcrumbs } from "~/components/molecules/custom-breadcrumbs";
-import { AppSidebar } from "~/components/organisms/app-sidebar";
-import { Button } from "~/components/ui/button";
 import { Separator } from "~/components/ui/separator";
-import { signOut } from "~/server/auth";
+import { ThemeToggle } from "~/components/atoms/theme-toggle";
+import { cookies } from "next/headers";
 import { db } from "~/server/db";
+import { redirect } from "next/navigation";
 
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth();
+  if (!session?.user) {
+    return redirect("/signin");
+  }
+
   const cookieStore = await cookies();
   const collaboratorId =
     cookieStore.get("selected-collaborator")?.value ?? null;
@@ -52,7 +58,7 @@ export default async function Layout({
         pedingEvaluations={pendingEvaluations}
       />
       <SidebarInset>
-        <header className="bg-background sticky top-0 z-50 flex h-16 max-h-16 grow items-center justify-between gap-2 border-b px-4 md:px-6 lg:px-8">
+        <header className="sticky top-0 z-50 flex h-16 max-h-16 grow items-center justify-between gap-2 border-b bg-background px-4 md:px-6 lg:px-8">
           <div className="flex items-center">
             <SidebarTrigger className="-ml-1 mr-1" />
             <Link
@@ -90,7 +96,7 @@ export default async function Layout({
           {children}
         </main>
         <footer className="flex">
-          <span className="bg-sidebar-primary text-sidebar-primary-foreground w-full py-1 text-center text-xs">
+          <span className="w-full bg-sidebar-primary py-1 text-center text-xs text-sidebar-primary-foreground">
             Coded with ❤️ by{" "}
             <Link
               href={"https://github.com/leonunesbs"}
